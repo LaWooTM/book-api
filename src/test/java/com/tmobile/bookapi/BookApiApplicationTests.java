3071bdea7de9b87d1ef081c3550999a543e9cfe3
Lauren Woods <lauren.woods31@t-mobile.com>
2020-12-15 15:54:46 -0500

    first commit

---
+++ src/test/java/com/tmobile/bookapi/BookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmobile.bookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
